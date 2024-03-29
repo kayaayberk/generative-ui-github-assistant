e820c93b1abd5a4fcf16046765806bb5bbfbef35
Ayberk Kaya <kayaayberk98@gmail.com>
2024-03-29 20:44:35 +0100

    refactor: change auth provider to Clerk -- ongoing Kinde Github authentication error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+    domains: [
+      'lh3.googleusercontent.com',
+      'avatars.githubusercontent.com',
+      'img.clerk.com',
+    ],
   },
 }
 
