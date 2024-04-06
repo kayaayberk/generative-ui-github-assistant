6b4b819c68b8ef63a90e70fa9878adcbf2d31996
Ayberk Kaya <kayaayberk98@gmail.com>
2024-04-06 23:12:05 +0200

    chore: update aiState on function call make llm aware of the ui state

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -3,15 +3,12 @@ import 'server-only'
 import {
   render,
   createAI,
+  getAIState,
   getMutableAIState,
   createStreamableValue,
-  getAIState,
 } from 'ai/rsc'
 import OpenAI from 'openai'
 
-import { saveChat } from '@/app/actions'
-import { InsertChat } from '@/db/schema'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import {
   BotCard,
   BotMessage,
@@ -21,17 +18,21 @@ import {
 import { z } from 'zod'
 import { Chat } from '../types'
 import { nanoid } from '../utils'
-import { ProfileSkeleton } from '@/components/assistant/ProfileSkeleton'
+import { saveChat } from '@/app/actions'
+import { currentUser } from '@clerk/nextjs'
+import { convertUserType, getGithubProfile } from './github/github'
 import { Profile } from '@/components/assistant/Profile'
-import { getGithubProfile, listUsers } from './github/github'
 import { ProfileList } from '@/components/assistant/ProfileList'
+import { ProfileSkeleton } from '@/components/assistant/ProfileSkeleton'
 import { useProfileConverter } from '../hooks/use-profile-converter'
+import { useId } from 'react'
+import { systemPrompt } from './system-prompt'
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY || '',
 })
 
-async function submitUserMessage(content: string) {
+async function submitUserMessage(content: string, attribute: string) {
   'use server'
 
   const aiState = getMutableAIState<typeof AI>()
@@ -45,12 +46,16 @@ async function submitUserMessage(content: string) {
         role: 'user',
         content,
       },
+      {
+        id: nanoid(),
+        role: 'system',
+        content: `[User has changed attribute to ${attribute}]`,
+      },
     ],
   })
 
   let textStream: undefined | ReturnType<typeof createStreamableValue<string>>
   let textNode: undefined | React.ReactNode
-
   const ui = render({
     model: 'gpt-3.5-turbo',
     provider: openai,
@@ -58,48 +63,7 @@ async function submitUserMessage(content: string) {
     messages: [
       {
         role: 'system',
-        content: `\
-      You are a GitHub search bot and you can help users find what they are looking for by searching the GitHub using GitHub API.
-      You and the user can discuss the search query and you can provide the user with the search results.
-      You can also provide the user with the search results from the GitHub API displayed in the UI.
-
-      Messages inside [] means that it's a UI element or a user event. For example:
-      - "[GitHub Profile of 'kayaayberk']" means that an interface of the user profile of 'kayaayberk' is shown to the user, 'kayaayberk' being the username of the searched user.
-      - "[User clicked on the 'repositories' button]" means that the user is being shown the repositories of the user whose profile is currently being displayed in the UI.
-      - "[Found repositories: 'repo1', 'repo2', 'repo3']" means that the search results are displayed to the user in the UI, 'repo1', 'repo2', 'repo3' being UI elements displaying the found repository details through the API search.
-      - "[Found related code snippet]" means that the related code snippet examples are displayed to the user in the UI.
-
-      If the user requests a single profile search on GitHub with a username, call \`show_user_profile_ui\` to show the found user profile UI. You shouln't show the information without calling this function. If the user does not provide a valid username, you can ask the user to provide a valid username before showing any content.
-      If user requests a list of users search on GitHub, call \`show_user_list_ui\` to show the found user list UI. You should only use this function to list users and should not show any data otherwise. This function requires querying so you should construct the query. Message inside () means query methdos. Following are the query methods you can use to construct the query:
-      - "(in)" to search for users in a specific location, login or email. For example: 
-      -- "(in:name)" to search for users with a specific name.
-      -- "(in:login)" to search for users with a specific username.
-      -- "(in:email)" to search for users with a specific email address.
-      Examples for (in) query methods: 
-      - "John+Doe+in:name" means that the user is searching for users with the name 'John Doe'.
-      - "johndoe+in:login" means that the user is searching for users with the username 'johndoe'.
-      - "johndoe@example.com+in:email" means that the user is searching for users with the email address'.
-
-      - "(repos)" to search for users with a specific number of repositories. For example:
-      -- "(repos:>10)" to search for users with more than 10 repositories.
-      -- "(repos:<10)" to search for users with less than 10 repositories.
-      -- "(repos:10..20)" to search for users with 10 to 20 repositories.
-      Examples for (repos) query methods:
-      - "johndoe+in:name+repos:>10" means that the user is searching for users with the name 'johndoe' and more than 10 repositories.
-      - "johndoe+in:login+repos:<10" means that the user is searching for users with the username 'johndoe' and less than 10 repositories.
-      - "johndoe+in:login+repos:10..20" means that the user is searching for users with the username 'johndoe' and 10 to 20 repositories.
-
-      - "(followers)" to search for users with a specific number of followers. For example:
-      -- "(followers:>10)" to search for users with more than 10 followers.
-      Examples for (followers) query methods:
-      - "johndoe+in:name+followers:>10+repos:>10" means that the user is searching for users with the name 'johndoe' and more than 10 followers and more than 10 repositories.
-      You can combine these query methods as it is in the followers example to construct a more detailed query based on the user's request.
-      
-      If the user requests a repository search on GitHub, call \`show_repository_ui\` to show the found repository UI.
-      If the user requests a code snippet search on GitHub, call \`show_code_snippet_ui\` to show the found code snippet UI.
-      If the user requests a general search on GitHub without a specific context, call \`show_general_search_ui\` to show the general search UI.
-
-      Besides that, you can also chat with users and do some calculations if needed.`,
+        content: systemPrompt,
       },
       ...aiState.get().messages.map((message: any) => ({
         role: message.role,
@@ -161,6 +125,18 @@ async function submitUserMessage(content: string) {
             ],
           })
 
+          aiState.update({
+            ...aiState.get(),
+            messages: [
+              ...aiState.get().messages,
+              {
+                id: nanoid(),
+                role: 'system' as const,
+                content: `[GitHub Profile of '${profile?.login}']`,
+              },
+            ],
+          })
+
           return (
             <BotCard>
               <Profile props={profile} />
@@ -180,6 +156,7 @@ async function submitUserMessage(content: string) {
             </BotCard>
           )
 
+          const profiles = await convertUserType(query)
           aiState.done({
             ...aiState.get(),
             messages: [
@@ -188,14 +165,25 @@ async function submitUserMessage(content: string) {
                 id: nanoid(),
                 role: 'function',
                 name: 'show_user_list_ui',
-                content: JSON.stringify(query),
+                content: JSON.stringify(profiles),
+              },
+            ],
+          })
+          aiState.update({
+            ...aiState.get(),
+            messages: [
+              ...aiState.get().messages,
+              {
+                id: nanoid(),
+                role: 'system' as const,
+                content: `[Found ${profiles.length} users with the usernames of '${profiles[0]?.login}', '${profiles[1]?.login}', '${profiles[2]?.login}', '${profiles[3]?.login}']`,
               },
             ],
           })
 
           return (
             <BotCard>
-              <ProfileList props={query} />
+              <ProfileList props={profiles} />
             </BotCard>
           )
         },
@@ -242,11 +230,9 @@ export const AI = createAI<AIState, UIState>({
   unstable_onGetUIState: async () => {
     'use server'
 
-    const { getUser, isAuthenticated } = getKindeServerSession()
-    const user = await getUser()
-    const session = await isAuthenticated()
+    const user = await currentUser()
 
-    if (session && user) {
+    if (user) {
       const aiState = getAIState()
 
       if (aiState) {
@@ -261,11 +247,9 @@ export const AI = createAI<AIState, UIState>({
   unstable_onSetAIState: async ({ state }) => {
     'use server'
 
-    const { getUser, isAuthenticated } = getKindeServerSession()
-    const user = await getUser()
-    const session = await isAuthenticated()
+    const user = await currentUser()
 
-    if (session && user) {
+    if (user) {
       const { chatId, messages } = state
 
       const createdAt = new Date()
@@ -315,3 +299,5 @@ export const getUIStateFromAIState = (aiState: Chat) => {
         ),
     }))
 }
+
+export const forceRole = (role: string) => {}
