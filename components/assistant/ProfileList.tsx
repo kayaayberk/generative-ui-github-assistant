927a3ee3ca359df957f3f40424832c8e59564c1e
Ayberk Kaya <kayaayberk98@gmail.com>
2024-04-10 22:45:08 +0200

    feat: add user profile UI actions

--- components/assistant/ProfileList.tsx
+++ components/assistant/ProfileList.tsx
@@ -1,18 +1,18 @@
 'use client'
 
-import { GithubUser, ListOfUsers } from '@/lib/types'
 import { Profile } from './Profile'
-import { useEffect, useState } from 'react'
-import { getGithubProfile, listUsers } from '@/lib/chat/github/github'
-import { set } from 'zod'
-import { useProfileConverter } from '@/lib/hooks/use-profile-converter'
+import { GithubUser } from '@/lib/types'
 
-export function ProfileList({ props: query }: { props: string }) {
-  const userDataArray = useProfileConverter(query)
-
+export function ProfileList({ props: profiles }: { props: GithubUser[] }) {
+  if (!Array.isArray(profiles)) {
+    return []
+  }
+  if (profiles.length === 0) {
+    return []
+  }
   return (
-    <div>
-      {userDataArray.map((user: GithubUser, index) => {
+    <div className='flex flex-col gap-2'>
+      {profiles.map((user: GithubUser, index) => {
         return <Profile key={index} props={user} />
       })}
     </div>
