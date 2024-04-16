9153ddd3411d3707397a29fa954c352ae72205ac
Ayberk Kaya <kayaayberk98@gmail.com>
2024-04-16 18:59:59 +0200

    chore: allow Next Image to accept all URLs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'lh3.googleusercontent.com',
-      'avatars.githubusercontent.com',
-      'img.clerk.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
     ],
   },
 }
