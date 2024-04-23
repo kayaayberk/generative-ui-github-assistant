aac36a4ca42d69a7c52309e173ec02250bcdbea3
Ayberk Kaya <kayaayberk98@gmail.com>
2024-04-23 22:59:23 +0200

    chore: edit public routes

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from '@clerk/nextjs'
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ['/', '/api/github'],
+  publicRoutes: ['/', '/sign-in'],
   // Allow signed out users to access the specified routes:
   // publicRoutes: ['/anyone-can-visit-this-route'],
 })
